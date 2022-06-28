f07eb3d6425bf3379761a1dd4a2966a980f28190
王鼎 <wangd_nc@landray.com.cn>
2022-06-28 11:29:58 +0800

    fix: #000 调整菜单权限

--- src/desktop/menu.ts
+++ src/desktop/menu.ts
@@ -1,13 +1,15 @@
 // 菜单配置
-export default [
+import api from '@/api/sysAuth'
+const menu = [
   /** 供应商管理*/
   {
     key: '/~~pc2wyyz6le8',
     label: ':menu.mpc2wyyz6le8',
     icon: 'add-document',
-    meta: {
-      role: ['ROLE_CMSOUTSUPPLIER_DEFAULT']
-    },
+    order: 1,
+    // meta: {
+    //   role: ['ROLE_CMSOUTSUPPLIER_DEFAULT']
+    // },
     children: [
       { key: '/cmsSupplierInfo/listSupplierInfo', label: ':menu.cmsSupplierInfo.listSupplierInfo' },
       // { key: '/cmsContractInfo/listContractInfo', label: ':menu.cmsContractInfo.listContractInfo' },
@@ -23,9 +25,10 @@ export default [
     key: '/cmsProjectInfo',
     label: ':menu.cmsProjectManagement',
     icon: 'add-document',
-    meta: {
-      role: ['ROLE_CMSOUTPROJECT_ADMIN']
-    },
+    order: 2,
+    // meta: {
+    //   role: ['ROLE_CMSOUTPROJECT_ADMIN']
+    // },
     children: [
       { key: '/cmsProjectInfo', label: ':menu.mbvr4bpjdbc', icon: 'add-document' },
     ]
@@ -35,9 +38,10 @@ export default [
     key: '/cmsStaffEntrance',
     label: ':menu.cmsPersonnelManagement',
     icon: 'add-document',
-    meta: {
-      role: ['ROLE_CMSOUTSTAFF_DEFAULT']
-    },
+    order: 3,
+    // meta: {
+    //   role: ['ROLE_CMSOUTSTAFF_DEFAULT']
+    // },
     children: [
       { key: '/cmsStaffEntrance', label: ':menu.mjeghuh20rd', icon: 'add-document' },
       { key: '/cmsStaffAdjust', label: ':menu.mdip4klbk717', icon: 'add-document' },
@@ -49,9 +53,10 @@ export default [
     key: '/cmsFrameInfo',
     label: ':menu.cmsBasicInformation',
     icon: 'add-document',
-    meta: {
-      role: ['ROLE_CMSOUTMANAGE_ADMIN']
-    },
+    order: 4,
+    // meta: {
+    //   role: ['ROLE_CMSOUTMANAGE_ADMIN']
+    // },
     children: [
       { key: '/cmsFrameInfo', label: ':menu.cmsFrameInfo', icon: 'add-document' },
       { key: '/cmsFrameBudget', label: ':menu.cmsFrameBudget', icon: 'add-document' },
@@ -65,9 +70,10 @@ export default [
     key: '/cmsOutOrder',
     label: ':menu.cmsWorkOrder',
     icon: 'add-document',
-    meta: {
-      role: ['ROLE_CMSOUTORDER_DEFAULT']
-    },
+    order: 5,
+    // meta: {
+    //   role: ['ROLE_CMSOUTORDER_DEFAULT']
+    // },
     children: [
       {
         key: '/cmsOutOrder',
@@ -82,3 +88,44 @@ export default [
     ]
   }
 ]
+const createMenu = async () => {
+  const params = [
+    {
+      status: 'checking',
+      key: 'auth0',
+      role: 'ROLE_CMSOUTSUPPLIER_DEFAULT'
+    },
+    {
+      status: 'checking',
+      key: 'auth1',
+      role: 'ROLE_CMSOUTPROJECT_ADMIN'
+    },
+    {
+      status: 'checking',
+      key: 'auth2',
+      role: 'ROLE_CMSOUTSTAFF_DEFAULT'
+    },
+    {
+      status: 'checking',
+      key: 'auth3',
+      role: 'ROLE_CMSOUTBASEDATA_ADMIN'
+    },
+    {
+      status: 'checking',
+      key: 'auth4',
+      role: 'ROLE_CMSOUTORDER_DEFAULT'
+    }
+  ]
+  const menuArr: any = []
+  const res = await api.roleCheck([...params])
+  Object.keys(res.data).forEach(i => {
+    if (res.data[i]) {
+      const num = i.split('h')[1]
+      menuArr.push(menu[num])
+    }
+  })
+  return menuArr
+}
+export default createMenu
+
+
