9f4e7bae9fdb024360512960d2b274f826238f3c
王鼎 <wangd_nc@landray.com.cn>
2022-06-28 18:16:55 +0800

    fix: #000 新增移动端form组件

--- src/desktop/menu.ts
+++ src/desktop/menu.ts
@@ -71,6 +71,7 @@ const menu = [
     label: ':menu.cmsWorkOrder',
     icon: 'add-document',
     order: 5,
+    module: 'cms-out-order',
     // meta: {
     //   role: ['ROLE_CMSOUTORDER_DEFAULT']
     // },
