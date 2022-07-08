adcbf90fa055907feda4c85976bd5096f73c7140
王鼎 <wangd_nc@landray.com.cn>
2022-07-08 10:45:07 +0800

    fix: #000 调整管理端菜单

--- src/manage/menu.ts
+++ src/manage/menu.ts
@@ -1,39 +1,61 @@
 // 菜单配置
 export default [
   {
-    key: '/cmsStaffAttConfigTemplate',
-    label: ':menu.cmsStaffAttConfigTemplate',
-    module: 'cms-out-staff',
-    icon: 'desktop'
+    key: '/cmsStaffLeaveTemplate',
+    label: 'cms-out-manage:menu.mpc2wyyz6le8',
+    icon: 'desktop',
+    children: [
+      {
+        key: '/cmsStaffReviewUpgradeTemplate',
+        label: 'cms-out-manage:menu.cmsStaffReviewUpgradeTemplate',
+        icon: 'desktop',
+        module: 'cms-out-supplier'
+      }
+    ]
   },
   {
     key: '/cmsStaffEntranceTemplate',
-    label: ':menu.cmsStaffEntranceTemplate',
-    module: 'cms-out-staff',
-    icon: 'desktop'
+    label: 'cms-out-manage:menu.cmsPersonnelManagement',
+    icon: 'desktop',
+    children: [
+      {
+        key: '/cmsStaffLeaveTemplate',
+        label: 'cms-out-manage:menu.cmsStaffLeaveTemplate',
+        module: 'cms-out-staff',
+        icon: 'desktop'
+      },
+      {
+        key: '/cmsStaffAttConfigTemplate',
+        label: 'cms-out-manage:menu.cmsStaffAttConfigTemplate',
+        module: 'cms-out-staff',
+        icon: 'desktop'
+      },
+      {
+        key: '/cmsStaffEntranceTemplate',
+        label: 'cms-out-manage:menu.cmsStaffEntranceTemplate',
+        module: 'cms-out-staff',
+        icon: 'desktop'
+      },
+      {
+        key: '/cmsStaffAdjustTemplate',
+        label: 'cms-out-manage:menu.cmsStaffAdjustTemplate',
+        module: 'cms-out-staff',
+        icon: 'desktop'
+      },
+    ]
   },
-  {
-    key: '/cmsStaffAdjustTemplate',
-    label: ':menu.cmsStaffAdjustTemplate',
-    module: 'cms-out-staff',
-    icon: 'desktop'
-  },
-  {
-    key: '/cmsStaffLeaveTemplate',
-    label: ':menu.cmsStaffLeaveTemplate',
-    module: 'cms-out-staff',
-    icon: 'desktop'
-  },
+
   {
     key: '/cmsOutConfig',
-    label: ':menu.cmsOutConfig',
+    label: 'cms-out-manage:menu.cmsWorkOrder',
     icon: 'desktop',
-    module: 'cms-out-order'
-  },
-  {
-    key: '/cmsStaffReviewUpgradeTemplate',
-    label: ':menu.cmsStaffReviewUpgradeTemplate',
-    icon: 'desktop',
-    module: 'cms-out-supplier'
-  },
+    children: [
+      {
+        key: '/cmsOutConfig',
+        label: 'cms-out-manage:menu.cmsOutConfig',
+        icon: 'desktop',
+        module: 'cms-out-order'
+      }
+    ]
+  }
 ]
