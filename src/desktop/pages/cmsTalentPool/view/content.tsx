d94c761ecbe28f36b07048f3fe1c6f82ab328510
王鼎 <wangd_nc@landray.com.cn>
2022-07-15 17:55:14 +0800

    fix: #000 菜单增加供应商服务

---
+++ src/desktop/pages/cmsTalentPool/view/content.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Module } from '@ekp-infra/common'
+import { Loading } from '@lui/core'
+
+const ViewContent = Module.getComponent('cms-out-supplier', 'CmsOutStaffInfoView', { loading: <Loading /> })
+
+const Content = (props) => {
+  return (
+    <ViewContent {...props} />
+  )
+}
+export default Content
