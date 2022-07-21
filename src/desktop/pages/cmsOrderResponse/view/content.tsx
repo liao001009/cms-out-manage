ca88d43d798777eb0b47596b7a2be39420a1d885
王鼎 <wangd_nc@landray.com.cn>
2022-07-21 18:06:59 +0800

    fix: #000 调整订单响应路由传参

--- src/desktop/pages/cmsOrderResponse/view/content.tsx
+++ src/desktop/pages/cmsOrderResponse/view/content.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Module } from '@ekp-infra/common'
 import { Loading } from '@lui/core'
 
-const AddContent = Module.getComponent('cms-out-project', 'CmsProjectWrittenAdd', { loading: <Loading /> })
+const ViewContent = Module.getComponent('cms-out-supplier', 'CmsOrderResponseView', { loading: <Loading /> })
 
 const Content = (props) => {
   return (
-    <AddContent {...props} />
+    <ViewContent {...props} />
   )
 }
 export default Content
