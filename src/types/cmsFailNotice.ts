f631045d96a44f5a515439de00b8282e833479ad
王鼎 <wangd_nc@landray.com.cn>
2022-09-23 16:24:22 +0800

    fix: #000 添加落选通知模块

---
+++ src/types/cmsFailNotice.ts
@@ -0,0 +1,42 @@
+import { IBaseType } from '@ekp-infra/common/dist/types'
+import { IOrgElement } from '@ekp-infra/common/dist/types'
+
+/*落选通知*/
+export interface ICmsFailNotice extends IBaseType {
+  //创建人
+  fdCreator: Partial<IOrgElement>
+  //创建时间
+  fdCreateTime: number
+  //创建人部门
+  fdCreatorDept: Partial<IOrgElement>
+  //修改人
+  fdAlter: Partial<IOrgElement>
+  //修改时间
+  fdAlterTime: number
+  //拥有者
+  fdOwner: Partial<IOrgElement>
+  //拥有者部门
+  fdOwnerDept: Partial<IOrgElement>
+  //文档状态
+  fdProcessStatus: string
+  //发布时间
+  fdPublishedTime: number
+  //流程模板ID
+  fdProcessTemplateId: string
+  //应用表单ID
+  fdXformId: string
+  //对应版本
+  fdVersion: string
+  //主键ID
+  fdId: string
+  //主题
+  fdSubject: string
+  //供应商名称
+  fdSupplier: any
+  //项目名称
+  fdProject: any
+  //通知内容
+  fdNotice: string
+  //中选信息
+  fdSelectedMation: any
+}
