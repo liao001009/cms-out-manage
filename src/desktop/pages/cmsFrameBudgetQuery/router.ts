3e294a3acca89db239bf504876bf191c91230d73
王鼎 <wangd_nc@landray.com.cn>
2022-10-18 14:51:17 +0800

    fix: #000 调整路由地址

--- src/desktop/pages/cmsFrameBudgetQuery/router.ts
+++ src/desktop/pages/cmsFrameBudgetQuery/router.ts
@@ -1,11 +1,11 @@
-import list from './list'
+// import list from './list'
 import AddCmpt from './add'
-import ViewCmpt from './view'
-import EditCmpt from './edit'
+// import ViewCmpt from './view'
+// import EditCmpt from './edit'
 
 export default {
   /** 路由前缀 */
-  router: '/cmsFrameBudget',
+  router: '/cmsFrameBudgetQuery',
   /** 页面配置，第一个为首页 */
-  pages: [AddCmpt, list, ViewCmpt, EditCmpt]
+  pages: [AddCmpt]
 }
