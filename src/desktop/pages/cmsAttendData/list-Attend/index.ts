1021266e9503a6842db97e71ac7b606ffe418af9
王鼎 <wangd_nc@landray.com.cn>
2022-10-20 10:51:05 +0800

    fix: #000 补全多语言

--- src/desktop/pages/cmsAttendData/list-Attend/index.ts
+++ src/desktop/pages/cmsAttendData/list-Attend/index.ts
@@ -5,7 +5,7 @@ export default {
   // 类型：模块
   type: 'page',
   // 页面标题
-  title: 'cms-out-manage:cmsAttendData.list.Attend',
+  title: 'cms-out-manage:menu.msiy3c4m0m8',
   // 路由
   router: '/listAttend',
   keepalive: false,
